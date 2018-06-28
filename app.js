e807ead05fce075d420952c30c2a7634f712b43d
Mike Hoskins <unixdoes@gmail.com>
2018-06-28 09:19:47 -0400

    refactor; enable middleware based on config

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 const express = require('express'),
     app = express(),
-    h = require('./helpers.js')
+    fs = require('fs'),
+    path = require('path'),
+    m = require('./middleware.js')
 
+const configFile = path.join(__dirname, process.env.CONF)
 const ip = process.env.IP || '0.0.0.0'
 const port = parseInt(process.env.PORT, 10) || 3000
 
+app.use(function (req, res, next) {
+    if (!req.app.locals.testResults) {
+        // hold aggregated test results
+        req.app.locals.testResults = {}
+    }
+    next()
+})
+
+// build list of tests to run based on config
+app.locals.conf = JSON.parse(fs.readFileSync(configFile, {encoding: 'utf-8'}))
+var tests = []
+
+if (app.locals.conf.testMongo) {
+    tests.push(m.testMongo)
+}
+if (app.locals.conf.testMysql) {
+    tests.push(m.testMysql)
+}
+if (app.locals.conf.testPostgres) {
+    tests.push(m.testPostgres)
+}
+if (app.locals.conf.testRabbit) {
+    tests.push(m.testRabbit)
+}
+if (app.locals.conf.testRedis) {
+    tests.push(m.testRedis)
+}
+
 // routes
-app.get('/', h.runTests, h.logRequest, function(req, res, next) {
-    res.send(res.locals.testResults)
+app.get('/', tests, function(req, res, next) {
+    //app.locals.testResults = '{ "message": "No tests enabled." }'
+    res.send(req.app.locals.testResults)
 })
 
 app.all('*', function(req, res, next) {
-    res.status(404)
-    // if ran as middleware this won't see res.status' effects
-    h.logRequest(req, res, next)
-    res.send({ 'error': 'Page not found.' })
+    res.status(404).send({ 'error': 'Page not found.' })
 })
 
 app.listen(port, ip, function() {
