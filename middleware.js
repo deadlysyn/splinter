073d153f9933bd56447cad1bb9b84d5fb2a47e77
Mike Hoskins <unixdoes@gmail.com>
2018-06-28 18:54:48 -0400

    add mongodb test; refactor test timing

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const cfenv = require('cfenv'),
     path = require('path'),
     mongoose = require('mongoose'),
+    Test = require('./models/mongoTest'),
     redis = require('redis'),
     mysql = require('mysql'),
     pg = require('pg')
@@ -13,38 +14,69 @@ const appEnv = cfenv.getAppEnv({
 var middleware = {}
 
 middleware.testMongo = function(req, res, next) {
+    let results = { message: 'success', time: 0 }
+    let credentials = appEnv.getServiceCreds(req.app.locals.conf.mongoInstance)
+    let currentTime = Date.now()
+
+    mongoose.connect(credentials.uri)
+    let db = mongoose.connection
+    db.on('error', console.error.bind(console, 'connection error:'))
+
+    // create document
+    let testDoc = new Test({ timestamp: currentTime })
+    testDoc.save(function (err) {
+        if (err) {
+            console.log(err)
+            res.status(500)
+            results.message = err.toString()
+        } else {
+            // find and read document
+            Test.findOne({ name: 'splinter' }, function(err, test) {
+                if (err) {
+                    console.log(err)
+                    res.status(500)
+                    results.message = err.toString()
+                } else {
+                    results.time = (Date.now() - test.timestamp) / 1000
+                }
+                req.app.locals.testResults.mongo = results
+                // cleanup
+                Test.remove({}, function(err) {
+                    if (err) {
+                        console.log(err)
+                    }
+                    db.close()
+                    return next()
+                })
+            })
+        }
+    })
 }
 
 middleware.testRedis = function(req, res, next) {
-    // test results
-    let results = {}
-
-    // debug...
-    console.log('req.app.locals: ' + JSON.stringify(req.app.locals))
-
+    let results = { message: 'success', time: 0 }
     let credentials = appEnv.getServiceCreds(req.app.locals.conf.redisInstance)
+    let currentTime = Date.now()
+
     let client = redis.createClient({
         host:       credentials.hostname,
         port:       credentials.port,
         password:   credentials.password
     })
 
-    // Log errors
     client.on('error', function (err) {
         console.log(err.toString());
     })
 
     // create record; auto-expire after 30 seconds
-    client.set('splinter', Date.now(), 'EX', 30)
+    client.set('splinter', currentTime, 'EX', 30)
 
     // read record
     client.get('splinter', function(err, timestamp) {
         if (err) {
             res.status(500)
             results.message = err.toString()
-            results.time = 0
         } else {
-            results.message = 'success'
             results.time = (Date.now() - timestamp) / 1000
         }
         client.quit()
