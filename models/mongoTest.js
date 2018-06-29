0743e79c31ed70b1c24d72276c7f971ba4d0d513
Mike Hoskins <unixdoes@gmail.com>
2018-06-29 00:38:45 -0400

    add default value for timestamp field

--- models/mongoTest.js
+++ models/mongoTest.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const testSchema = new mongoose.Schema({
     name: { type: String, default: 'splinter' },
-    timestamp: Number
+    timestamp: { type: Number, default: 0 }
 })
 
 module.exports = mongoose.model('Test', testSchema)
