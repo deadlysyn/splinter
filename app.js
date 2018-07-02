488c76721272deaf34db2d25a550da45361234d6
Mike Hoskins <unixdoes@gmail.com>
2018-07-01 21:45:32 -0400

    fix typo

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ if (tests.length == 0) {
 
 // routes
 app.get('/', tests, function(req, res, next) {
-    res.send.json({
+    res.json({
         'timestamp': new Date().toJSON(),
         'results': req.app.locals.testResults
     })
