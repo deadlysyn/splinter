b21add6d4b7d6afa49e1e970521be2e39fe0637b
Mike Hoskins <unixdoes@gmail.com>
2018-07-18 23:07:52 -0400

    refactor testMongo

--- middleware/index.js
+++ middleware/index.js
@@ -49,43 +49,45 @@ function randName() {
 var middleware = {}
 
 middleware.testMongo = (req, res, next) => {
-    let mongoose = require('mongoose')
-    let Test = require('../models/mongoTest')
     let svc = req.app.locals.conf.mongoInstance
     let cfg = init(req, res, svc)
     let name = randName()
 
-    let cleanup = _ => {
-        Test.remove({}, _ => {
-            db.close()
-            return next()
-        })
-    }
-
-    mongoose.connect(cfg.creds.uri, { 'bufferCommands': false })
-    let db = mongoose.connection
-    db.on('error', (err) => handleErr(err, cfg, cleanup))
-
+    let mongoose = require('mongoose')
+    let Test = require('../models/mongoTest')
     let testDoc = new Test({
         'name': name,
         'timestamp': cfg.time
     })
 
-    testDoc.save((err) => {
+    let cleanup = _ => {
+        Test.remove({}, _ => {
+            mongoose.connection.close()
+            return next()
+        })
+    }
+
+    mongoose.connect(cfg.creds.uri, { 'bufferCommands': false }, err => {
         if (err) {
             handleErr(err, cfg, cleanup)
         } else {
-            Test.findOne({ name: name }, (err, test) => {
+            testDoc.save((err) => {
                 if (err) {
                     handleErr(err, cfg, cleanup)
                 } else {
-                    if (test) {
-                        cfg.results.seconds_elapsed = (Date.now() - test.timestamp) / 1000
-                        req.app.locals.testResults[svc] = cfg.results
-                        cleanup()
-                    } else {
-                        handleErr('Error: No document found', cfg, cleanup)
-                    }
+                    Test.findOne({ name: name }, (err, test) => {
+                        if (err) {
+                            handleErr(err, cfg, cleanup)
+                        } else {
+                            if (test) {
+                                cfg.results.seconds_elapsed = (Date.now() - test.timestamp) / 1000
+                                req.app.locals.testResults[svc] = cfg.results
+                                cleanup()
+                            } else {
+                                handleErr('Error: No document found', cfg, cleanup)
+                            }
+                        }
+                    })
                 }
             })
         }
