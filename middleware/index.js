52bbaa6a88415bd812d5dc15a8b7936a56bb08c9
Mike Hoskins <unixdoes@gmail.com>
2019-02-28 22:48:10 -0500

    refactor; start prometheus exporter format

--- middleware/index.js
+++ middleware/index.js
@@ -1,277 +1,277 @@
-const cfenv = require('cfenv')
-const path = require('path')
+/* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["cfg", "req"] }] */
+/* eslint no-shadow: ["error", { "allow": ["err"] }] */
+
+const cfenv = require('cfenv');
+const path = require('path');
+const mongoose = require('mongoose');
+const mysql = require('mysql');
+const pg = require('pg');
+const rabbit = require('amqplib/callback_api');
+const redis = require('redis');
+const Test = require('../models/mongoTest');
 
 // parse VCAP_SERVICES. vcapFile used when ran locally.
 const appEnv = cfenv.getAppEnv({
-    "vcapFile": path.join(__dirname, '../test/vcap.json')
-})
-
-/* helpers */
+  vcapFile: path.join(__dirname, '../test/vcap.json'),
+});
 
 // common tasks run on any errors
 function handleErr(err, cfg, callback) {
-    console.log(err)
-    cfg.res.status(500)
-    cfg.results.message = err.toString()
-    cfg.req.app.locals.testResults[cfg.svc] = cfg.results
-    return callback()
+  // eslint-disable-next-line no-console
+  console.log(err);
+  cfg.res.status(500);
+  cfg.results.message = err.toString();
+  cfg.req.app.locals.testResults[cfg.svc] = cfg.results;
+  return callback();
 }
 
 // build config object for test
 function init(req, res, svc) {
-    return {
-        creds: appEnv.getServiceCreds(svc),
-        req: req,
-        res: res,
-        svc: svc,
-        time: Date.now(),
-        results: {
-            message: 'success',
-            seconds_elapsed: -255
-        }
-    }
+  return {
+    creds: appEnv.getServiceCreds(svc),
+    req,
+    res,
+    svc,
+    time: Date.now(),
+    results: {
+      message: 'success',
+      seconds_elapsed: -255,
+    },
+  };
 }
 
 // generate random names for test tables, queues, etc.
 function randName() {
-    let name = 'splinter' // prefix
-    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let name = 'splinter'; // prefix
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (let i = 0; i < 8; i++) {
-        name += chars.charAt(Math.floor(Math.random() * chars.length))
-    }
+  for (let i = 0; i < 8; i + 1) {
+    name += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
 
-    return name
+  return name;
 }
 
-/* middleware */
-
-var middleware = {}
+const middleware = {};
 
 middleware.testMongo = (req, res, next) => {
-    let svc = req.app.locals.conf.mongoInstance
-    let cfg = init(req, res, svc)
-    let name = randName()
+  const svc = req.app.locals.conf.mongoInstance;
+  const cfg = init(req, res, svc);
+  const name = randName();
 
-    let mongoose = require('mongoose')
-    let Test = require('../models/mongoTest')
-    let testDoc = new Test({
-        'name': name,
-        'timestamp': cfg.time
-    })
+  const testDoc = new Test({
+    name,
+    timestamp: cfg.time,
+  });
 
-    let cleanup = _ => {
-        Test.remove({}, _ => {
-            mongoose.connection.close()
-            return next()
-        })
+  const cleanup = _ => {
+    Test.remove({}, () => {
+      mongoose.connection.close();
+      return next();
+    });
+  };
+
+  mongoose.connect(
+    cfg.creds.uri,
+    { bufferCommands: false },
+    err => {
+      if (err) {
+        handleErr(err, cfg, cleanup);
+      } else {
+        testDoc.save(err => {
+          if (err) {
+            handleErr(err, cfg, cleanup);
+          } else {
+            Test.findOne({ name }, (err, test) => {
+              if (err) {
+                handleErr(err, cfg, cleanup);
+              } else if (test) {
+                cfg.results.seconds_elapsed = (Date.now() - test.timestamp) / 1000;
+                req.app.locals.testResults[svc] = cfg.results;
+                cleanup();
+              } else {
+                handleErr('Error: No document found', cfg, cleanup);
+              }
+            });
+          }
+        });
+      }
     }
-
-    mongoose.connect(cfg.creds.uri, { 'bufferCommands': false }, err => {
-        if (err) {
-            handleErr(err, cfg, cleanup)
-        } else {
-            testDoc.save(err => {
-                if (err) {
-                    handleErr(err, cfg, cleanup)
-                } else {
-                    Test.findOne({ name: name }, (err, test) => {
-                        if (err) {
-                            handleErr(err, cfg, cleanup)
-                        } else {
-                            if (test) {
-                                cfg.results.seconds_elapsed = (Date.now() - test.timestamp) / 1000
-                                req.app.locals.testResults[svc] = cfg.results
-                                cleanup()
-                            } else {
-                                handleErr('Error: No document found', cfg, cleanup)
-                            }
-                        }
-                    })
-                }
-            })
-        }
-    })
-}
+  );
+};
 
 middleware.testMysql = (req, res, next) => {
-    let svc = req.app.locals.conf.mysqlInstance
-    let cfg = init(req, res, svc)
-    let tbl = randName()
+  const svc = req.app.locals.conf.mysqlInstance;
+  const cfg = init(req, res, svc);
+  const tbl = randName();
 
-    let mysql = require('mysql')
-    let db = mysql.createConnection(cfg.creds.uri)
+  const db = mysql.createConnection(cfg.creds.uri);
 
-    let cleanup = _ => {
-        db.query('DROP TABLE ??', tbl, _ => {
-            db.destroy()
-            return next()
-        })
-    }
+  const cleanup = _ => {
+    db.query('DROP TABLE ??', tbl, () => {
+      db.destroy();
+      return next();
+    });
+  };
 
-    db.connect(err => {
+  db.connect(err => {
+    if (err) {
+      handleErr(err, cfg, cleanup);
+    } else {
+      db.query('CREATE TABLE ?? (timestamp BIGINT)', tbl, err => {
         if (err) {
-            handleErr(err, cfg, cleanup)
+          handleErr(err, cfg, cleanup);
         } else {
-            db.query('CREATE TABLE ?? (timestamp BIGINT)', tbl, err => {
+          db.query('INSERT INTO ?? (timestamp) VALUES(?)', [tbl, cfg.time], err => {
+            if (err) {
+              handleErr(err, cfg, cleanup);
+            } else {
+              db.query('SELECT timestamp FROM ?? LIMIT 1', tbl, (err, result) => {
                 if (err) {
-                    handleErr(err, cfg, cleanup)
+                  handleErr(err, cfg, cleanup);
+                } else if (result) {
+                  cfg.results.seconds_elapsed = (Date.now() - result[0].timestamp) / 1000;
+                  req.app.locals.testResults[svc] = cfg.results;
+                  cleanup();
                 } else {
-                    db.query('INSERT INTO ?? (timestamp) VALUES(?)', [tbl, cfg.time], err => {
-                        if (err) {
-                            handleErr(err, cfg, cleanup)
-                        } else {
-                            db.query('SELECT timestamp FROM ?? LIMIT 1', tbl, (err, result) => {
-                                if (err) {
-                                    handleErr(err, cfg, cleanup)
-                                } else {
-                                    if (result) {
-                                        cfg.results.seconds_elapsed = (Date.now() - result[0].timestamp) / 1000
-                                        req.app.locals.testResults[svc] = cfg.results
-                                        cleanup()
-                                    } else {
-                                        handleErr('Error: No results from query', cfg, cleanup)
-                                    }
-                                }
-                            })
-                        }
-                    })
+                  handleErr('Error: No results from query', cfg, cleanup);
                 }
-            })
+              });
+            }
+          });
         }
-
-    })
-}
+      });
+    }
+  });
+};
 
 middleware.testPostgres = (req, res, next) => {
-    let svc = req.app.locals.conf.postgresInstance
-    let cfg = init(req, res, svc)
-    let tbl = randName()
+  const svc = req.app.locals.conf.postgresInstance;
+  const cfg = init(req, res, svc);
+  const tbl = randName();
 
-    let pg = require('pg')
-    let db = new pg.Client({ connectionString: cfg.creds.uri })
+  const db = new pg.Client({ connectionString: cfg.creds.uri });
 
-    let cleanup = _ => {
-        db.query(`DROP TABLE ${tbl}`, _ => {
-            db.end()
-            return next()
-        })
-    }
+  const cleanup = _ => {
+    db.query(`DROP TABLE ${tbl}`, () => {
+      db.end();
+      return next();
+    });
+  };
 
-    db.connect(err => {
+  db.connect(err => {
+    if (err) {
+      handleErr(err, cfg, cleanup);
+    } else {
+      db.query(`CREATE TABLE ${tbl} (timestamp BIGINT)`, err => {
         if (err) {
-            handleErr(err, cfg, cleanup)
+          handleErr(err, cfg, cleanup);
         } else {
-            db.query(`CREATE TABLE ${tbl} (timestamp BIGINT)`, err => {
+          db.query(`INSERT INTO ${tbl} (timestamp) VALUES($1)`, [cfg.time], err => {
+            if (err) {
+              handleErr(err, cfg, cleanup);
+            } else {
+              db.query(`SELECT timestamp FROM ${tbl} LIMIT 1`, (err, result) => {
                 if (err) {
-                    handleErr(err, cfg, cleanup)
+                  handleErr(err, cfg, cleanup);
+                } else if (result) {
+                  cfg.results.seconds_elapsed = (Date.now() - result.rows[0].timestamp) / 1000;
+                  req.app.locals.testResults[svc] = cfg.results;
+                  cleanup();
                 } else {
-                    db.query(`INSERT INTO ${tbl} (timestamp) VALUES($1)`, [cfg.time], err => {
-                        if (err) {
-                            handleErr(err, cfg, cleanup)
-                        } else {
-                            db.query(`SELECT timestamp FROM ${tbl} LIMIT 1`, (err, result) => {
-                                if (err) {
-                                    handleErr(err, cfg, cleanup)
-                                } else {
-                                    if (result) {
-                                        cfg.results.seconds_elapsed = (Date.now() - result.rows[0].timestamp) / 1000
-                                        req.app.locals.testResults[svc] = cfg.results
-                                        cleanup()
-                                    } else {
-                                        handleErr('Error: No results from query', cfg, cleanup)
-                                    }
-                                }
-                            })
-                        }
-                    })
+                  handleErr('Error: No results from query', cfg, cleanup);
                 }
-            })
+              });
+            }
+          });
         }
-    })
-}
+      });
+    }
+  });
+};
 
 middleware.testRabbit = (req, res, next) => {
-    let svc = req.app.locals.conf.rabbitInstance
-    let cfg = init(req, res, svc)
-    let q = randName()
+  const svc = req.app.locals.conf.rabbitInstance;
+  const cfg = init(req, res, svc);
+  const q = randName();
 
-    let rabbit = require('amqplib/callback_api')
-
-    // may be called before conn or ch are defined
-    let cleanup = _ => {
-        let finish = _ => {
-            try {
-                conn.close()
-                return next()
-            } catch(err) {
-                return next()
-            }
-        }
-        try {
-            ch.deleteQueue(q, {}, (err, ok) => {
-                finish()
-            })
-        } catch(err) {
-            finish()
-        }
+  // may be called before conn or ch are defined
+  const cleanup = () => {
+    const finish = () => {
+      try {
+        conn.close();
+        return next();
+      } catch (err) {
+        return next();
+      }
+    };
+    try {
+      ch.deleteQueue(q, {}, (err, ok) => {
+        finish();
+      });
+    } catch (err) {
+      finish();
     }
+  };
 
-    rabbit.connect(cfg.creds.uri, { noDelay: true }, (err, conn) => {
-        if (err) {
-            handleErr(err, cfg, cleanup)
-        } else {
-            conn.createChannel((err, ch) => {
-                if (err) {
-                    handleErr(err, cfg, cleanup)
-                } else {
-                    ch.assertQueue(q, { exclusive: true, autoDelete: true }, (err, ok) => {
-                        if (err) {
-                            handleErr(err, cfg, cleanup)
-                        } else {
-                            ch.sendToQueue(q, Buffer.from(cfg.time.toString()))
-                            ch.consume(q, (msg) => {
-                                cfg.results.seconds_elapsed = (Date.now() - Number(msg.content.toString())) / 1000
-                                req.app.locals.testResults[svc] = cfg.results
-                                ch.ackAll()
-                                cleanup()
-                            })
-                        }
-                    })
-                }
-            })
-        }
-    })
-}
+  rabbit.connect(
+    cfg.creds.uri,
+    { noDelay: true },
+    (err, conn) => {
+      if (err) {
+        handleErr(err, cfg, cleanup);
+      } else {
+        conn.createChannel((err, ch) => {
+          if (err) {
+            handleErr(err, cfg, cleanup);
+          } else {
+            ch.assertQueue(q, { exclusive: true, autoDelete: true }, (err, ok) => {
+              if (err) {
+                handleErr(err, cfg, cleanup);
+              } else {
+                ch.sendToQueue(q, Buffer.from(cfg.time.toString()));
+                ch.consume(q, msg => {
+                  cfg.results.seconds_elapsed = (Date.now() - Number(msg.content.toString())) / 1000;
+                  req.app.locals.testResults[svc] = cfg.results;
+                  ch.ackAll();
+                  cleanup();
+                });
+              }
+            });
+          }
+        });
+      }
+    }
+  );
+};
 
 middleware.testRedis = (req, res, next) => {
-    let svc = req.app.locals.conf.redisInstance
-    let cfg = init(req, res, svc)
-    let q = randName()
+  const svc = req.app.locals.conf.redisInstance;
+  const cfg = init(req, res, svc);
+  const q = randName();
 
-    let redis = require('redis')
-    let client = redis.createClient({
-        host:       cfg.creds.hostname,
-        port:       cfg.creds.port,
-        password:   cfg.creds.password
-    })
+  const client = redis.createClient({
+    host: cfg.creds.hostname,
+    port: cfg.creds.port,
+    password: cfg.creds.password,
+  });
 
-    let cleanup = _ => {
-        client.quit()
-        return next()
+  const cleanup = () => {
+    client.quit();
+    return next();
+  };
+
+  client.on('error', err => handleErr(err, cfg, cleanup));
+  client.set(q, cfg.time, 'EX', 30); // expire after 30 seconds
+  client.get(q, (err, timestamp) => {
+    if (err) {
+      handleErr(err, cfg, cleanup);
+    } else {
+      cfg.results.seconds_elapsed = (Date.now() - timestamp) / 1000;
+      req.app.locals.testResults[svc] = cfg.results;
+      cleanup();
     }
+  });
+};
 
-    client.on('error', (err) => handleErr(err, cfg, cleanup))
-    client.set(q, cfg.time, 'EX', 30) // expire after 30 seconds
-    client.get(q, (err, timestamp) => {
-        if (err) {
-            handleErr(err, cfg, cleanup)
-        } else {
-            cfg.results.seconds_elapsed = (Date.now() - timestamp) / 1000
-            req.app.locals.testResults[svc] = cfg.results
-            cleanup()
-        }
-    })
-}
-
-module.exports = middleware
+module.exports = middleware;
