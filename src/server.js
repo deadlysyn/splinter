7f5559c24ffc8dd6a99f63d34fc9d08738991af7
Mike Hoskins <mrh@devopsdreams.io>
2019-10-26 19:49:52 -0400

    cleanup

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const ip = process.env.IP || '0.0.0.0'
 const port = process.env.PORT || 3000
 
 app.listen(port, ip, () => {
-  console.log(`listening on port ${ip}:${port}...`)
+  console.log(`INFO - Listening on ${ip}:${port}`)
 })
