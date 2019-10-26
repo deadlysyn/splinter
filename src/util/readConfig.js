8c3f3be42cb7d4a86f9995540ee08b7b5bd28b24
Mike Hoskins <mhoskins@pivotal.io>
2019-10-26 19:43:13 -0400

    cleanup

--- src/util/readConfig.js
+++ src/util/readConfig.js
@@ -5,12 +5,15 @@ const configFile = `${__dirname}/../../${process.env.CONFIG}`
 let enabledTests = []
 
 try {
+  console.log(`INFO - reading configuration from ${configFile}`)
+
   const config = yaml.safeLoad(fs.readFileSync(configFile, 'utf8'))
+
   if (config) {
     enabledTests = config.tests.filter(test => test.enabled === true)
   }
-} catch (e) {
-  console.log(`ERROR - ${e.message}`)
+} catch (error) {
+  console.log(`ERROR - ${error.message}`)
 }
 
 module.exports = enabledTests
