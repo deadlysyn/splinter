cc95afc9212a4470aabe15b11ad047a8ec31eefb
Mike Hoskins <mhoskins@pivotal.io>
2019-10-31 17:21:03 -0400

    expose mongodb test metrics via prometheus

--- src/testers/mongodb.js
+++ src/testers/mongodb.js
@@ -2,13 +2,14 @@ const uuid = require('uuid/v1')
 const dbConnect = require('../db/mongodb')
 const Test = require('../../models/mongoTest')
 const { init, handleError, getCreds } = require('../util/helpers')
+const { requests, errors, latency } = require('../metrics/mongodb')
 
 const testMongo = async instance => {
   const testState = init(instance)
   const name = uuid()
 
   const { error, db } = await dbConnect(getCreds(instance))
-  if (error) return handleError({ testState, error })
+  if (error) return handleError({ testState, error, errors })
 
   try {
     const testDoc = new Test({
@@ -20,8 +21,10 @@ const testMongo = async instance => {
     const test = await Test.findOne({ name })
     if (!test) throw new Error('Unable to retrieve document.')
     testState.results.secondsElapsed = (Date.now() - test.startTime) / 1000
+    requests.inc()
+    latency.observe(testState.results.secondsElapsed)
   } catch (error) {
-    handleError({ testState, error })
+    handleError({ testState, error, errors })
   } finally {
     await Test.deleteMany({})
     await db.connection.close()
