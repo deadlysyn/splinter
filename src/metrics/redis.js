2df906bcf302a04c2affa56a6406a0270b17e93d
Mike Hoskins <mhoskins@pivotal.io>
2019-10-31 17:51:22 -0400

    fix prometheus import path and make metric names unique

--- src/metrics/redis.js
+++ src/metrics/redis.js
@@ -1,22 +1,22 @@
 const prometheus = require('prom-client')
 
-const requests = new prometheus.Counter({
+const requestsRedis = new prometheus.Counter({
   name: 'splinter_redis_tests_total',
   help: 'Total Redis tests across process lifetime.',
 })
 
-const errors = new prometheus.Counter({
+const errorsRedis = new prometheus.Counter({
   name: 'splinter_redis_errors_total',
   help: 'Total Redis errors across process lifetime.',
 })
 
-const latency = new prometheus.Histogram({
+const latencyRedis = new prometheus.Histogram({
   name: 'splinter_redis_latency_seconds',
   help: 'Redis test latency.',
 })
 
 module.exports = {
-  requests,
-  errors,
-  latency,
+  requests: requestsRedis,
+  errors: errorsRedis,
+  latency: latencyRedis,
 }
