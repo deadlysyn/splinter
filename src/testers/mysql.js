e2c83da62546895849582bc09c2c3f20b3ff4597
Mike Hoskins <mrh@devopsdreams.io>
2019-10-31 17:27:16 -0400

    expose mysql test metrics via prometheus

--- src/testers/mysql.js
+++ src/testers/mysql.js
@@ -1,13 +1,14 @@
 const uuid = require('uuid/v1')
 const dbConnect = require('../db/mysql')
 const { init, handleError, getCreds } = require('../util/helpers')
+const { requests, errors, latency } = require('../metrics/mongodb')
 
 const testMysql = async instance => {
   const testState = init(instance)
   const table = uuid()
 
   const { error, db } = await dbConnect(getCreds(instance))
-  if (error) return handleError({ testState, error })
+  if (error) return handleError({ testState, error, errors })
 
   // template strings since data is not user-provided
   const queryCreate = `CREATE TABLE IF NOT EXISTS \`${table}\` (start_time BIGINT)`
@@ -21,8 +22,10 @@ const testMysql = async instance => {
     const [rows, fields] = await db.query(querySelect)
     if (rows.length === 0) throw new Error('No rows retrieved from database.')
     testState.results.secondsElapsed = (Date.now() - rows[0].start_time) / 1000
+    requests.inc()
+    latency.observe(testState.results.secondsElapsed)
   } catch (error) {
-    handleError({ testState, error })
+    handleError({ testState, error, errors })
   } finally {
     await db.query(queryDrop)
     await db.end()
