8bbc2b90008e4650993095b340241d9a73ed5239
Mike Hoskins <mhoskins@pivotal.io>
2019-10-31 17:20:31 -0400

    add metrics endpoint and expose default metrics

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const prometheus = require('prom-client')
 const runTests = require('./util/runTests')
 
 const app = express()
 app.use(express.json())
+
+prometheus.collectDefaultMetrics({ prefix: 'splinter_' })
 
 app.get('/', runTests, (req, res) => {
   const errorsDetected = res.locals.testResults.some(test => test.message !== 'OK')
   let statusCode = 200
   if (errorsDetected) statusCode = 502
   res.status(statusCode).send({ results: res.locals.testResults })
+})
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', prometheus.register.contentType)
+  res.end(prometheus.register.metrics())
 })
 
 app.all('*', (req, res) => {
